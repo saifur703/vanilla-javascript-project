f83c3c32aceb334c72f7b0d0d6d77aa262453756
Saifur Rahman <saifur703@gmail.com>
2022-04-06 22:46:34 +0600

    add dynamic Title for Light On Off

--- bulb-on-bulb-off/script.js
+++ bulb-on-bulb-off/script.js
@@ -1,14 +1,17 @@
 let img = document.querySelectorAll(".lights img");
 let lightOn = document.querySelector("#lightOn");
 let lightOff = document.querySelector("#lightOff");
+let title = document.querySelector(".title");
 
 lightOn.addEventListener("click", function () {
   for (let i = 0; i < img.length; i++) {
     img[i].src = "pic_bulbOn.gif";
+    title.innerHTML = "Light ON";
   }
 });
 lightOff.addEventListener("click", function () {
   for (let i = 0; i < img.length; i++) {
     img[i].src = "pic_bulbOff.gif";
+    title.innerHTML = "Light OFF";
   }
 });
