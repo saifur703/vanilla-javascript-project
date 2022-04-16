69ab6047ff6ca7e2cf46c301b784c2c7026a2b66
Saifur Rahman <saifur703@gmail.com>
2022-04-16 22:27:37 +0600

    popup

--- popup/active.js
+++ popup/active.js
@@ -3,6 +3,7 @@ let close = document.querySelector(".close");
 let popupBtn = document.querySelector(".popupBtn");
 let body = document.querySelector("body");
 
+// Load Popup after 2 seconds
 window.addEventListener("load", function () {
   setTimeout(function () {
     modal.classList.add("active");
@@ -10,12 +11,27 @@ window.addEventListener("load", function () {
   }, 2000);
 });
 
+// Close Events
 close.addEventListener("click", function () {
   modal.classList.remove("active");
   body.classList.remove("modal-overlay");
 });
 
+// Button Click Show Popup Event
 popupBtn.addEventListener("click", function () {
   modal.classList.add("active");
   body.classList.add("modal-overlay");
+});
+
+// Click outside Event
+document.addEventListener("click", function (event) {
+  var isClickInsideModal = modal.contains(event.target);
+  var isClickInsidePopupBtn = popupBtn.contains(event.target);
+
+  if (!isClickInsideModal) {
+    if (!isClickInsidePopupBtn) {
+      modal.classList.remove("active");
+      body.classList.remove("modal-overlay");
+    }
+  }
 });
